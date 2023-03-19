4d2e07c0e39fdaffaa90563f28fef396e5dbcc42
AJ <amarjitbhandal@gmail.com>
2023-03-19 21:33:16 +1100

    Add Movie logic, add json-server Application complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,54 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {MovieType} from "./components/Movie";
 import MovieList from "./components/MovieList";
 // @ts-ignore
 import * as classes from './App.css';
 import {fetchMovies} from "./FetchMovies";
+import AddMovie from "./components/AddMovie";
 
 function App() {
-
     const [dummyMovies, setDummyMovies] = useState<MovieType[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<ErrorOptions>({});
+    const firstTimeRender = useRef<boolean>(true);
 
+    const moviesUrl = 'http://localhost:3500/movies';
 
-    const fetchMoviesHandler = fetchMovies(setIsLoading, setDummyMovies, setError);
+    useEffect(() => {
+        console.log('Run useEffect');
+        if (firstTimeRender.current) {
+            fetchMovies(setIsLoading, setDummyMovies, setError)();
+            firstTimeRender.current = false;
+        }
+        return () => {
+            console.log('clean up');
+        };
+    }, []);
 
-
-    const errorMessage = `Error...${error.cause}`;
-
+    const addMovieHandler = (movie: MovieType) => {
+        console.log(movie);
+        fetch(moviesUrl, {
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
 
   return (
       <React.Fragment>
+          <section className={classes.section}>
+          <AddMovie addMovieHandler={addMovieHandler} />
+          </section>
         <section className={classes.section}>
-          <button className={classes.button} onClick={fetchMoviesHandler}>Fetch Movies</button>
+          <button className={classes.button} onClick={fetchMovies(setIsLoading, setDummyMovies, setError)}>Fetch Movies</button>
         </section>
         <section className={classes.section}>
             {!isLoading && dummyMovies.length > 0 && <MovieList movies={dummyMovies} />}
             {!isLoading && dummyMovies.length === 0 && <p>No movies to display..</p>}
             {isLoading && <p>Loading movies...</p>}
-            {!isLoading && error.cause !== undefined && <p>{errorMessage}</p>}
+            {!isLoading && error.cause !== undefined && <p>{`Error...${error.cause}`}</p>}
         </section>
       </React.Fragment>
   );
